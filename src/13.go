4c9a3569ce63647fd2e2c37cbfa3108eefe144fc
ayeleennaria <ayeleennaria@gmail.com>
2025-03-18 23:00:56 +0000

    Add 13.go

---
+++ src/13.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alice"
+	age := 30
+	city := "New York"
+
+	fmt.Println("My name is", name)
+	fmt.Println("I am", age, "years old")
+	fmt.Println("I live in", city)
+}
